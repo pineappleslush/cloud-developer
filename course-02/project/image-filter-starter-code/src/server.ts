1c1bfb2d32d2caad9908f89b303baf259cd59478
lani <lanimorton356@gmail.com>
2021-08-15 13:08:22 -0700

    Image Filter Project

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -17,7 +17,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -28,7 +27,16 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get("/filteredimage", async (req, res) => {
+    let imageUrl = req.query.image_url;
 
+    if (!imageUrl) {
+      return res.status(401).send({ message: 'No URL provided.' });
+    }
+
+    const filteredImagePath = await filterImageFromURL(imageUrl);
+    res.type('jpg').sendFile(filteredImagePath);
+  });
   //! END @TODO1
   
   // Root Endpoint
